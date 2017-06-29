d0a48da287e1396723067be7abd8386c9275cb51
David Lampon <david.lampon@netcentric.biz>
2017-06-29 11:38:26 +0200

    Enabled config.js tests

--- test/test.js
+++ test/test.js
@@ -3,43 +3,42 @@
 const expect = require('chai').expect;
 const should = require('chai').should();
 
-// const configFile = require('../config');
+const configFile = require('../config');
 const flickrInterface = require('../index');
 
-// describe('#configFile', () => {
-//   it('should exist', () => {
-//     should.exist(configFile);
-//   });
+describe('#configFile', () => {
+  it('should exist', () => {
+    should.exist(configFile);
+  });
 
-//   it('should be an object', () => {
-//     expect(configFile).to.be.an('object');
-//   });
+  it('should be an object', () => {
+    expect(configFile).to.be.an('object');
+  });
 
-//   it('should have a personal ID', () => {
-//     expect(configFile).to.have.own.property('personalId').to.be.a('string').to.not.be.empty;
-//   });
+  it('should have a personal ID', () => {
+    expect(configFile).to.have.own.property('personalId').to.be.a('string').to.not.be.empty;
+  });
 
-//   it('should have a Flickr object', () => {
-//     expect(configFile).to.have.own.property('flickr');
-//   });
+  it('should have a Flickr object', () => {
+    expect(configFile).to.have.own.property('flickr');
+  });
 
-//   it('should have a Flickr object with all properties properly set', () => {
-//     const flickrObject = configFile.flickr;
-//     expect(flickrObject).to.have.own.property('apiKey').to.be.a('string').to.not.be.empty;
-//     expect(flickrObject).to.have.own.property('apiSecret').to.be.a('string').to.not.be.empty;
-//     expect(flickrObject).to.have.own.property('accessToken').to.be.a('string').to.not.be.empty;
-//     expect(flickrObject)
-//      .to.have.own.property('accessTokenSecret').to.be.a('string').to.not.be.empty;
-//   });
+  it('should have a Flickr object with all properties properly set', () => {
+    const flickrObject = configFile.flickr;
+    expect(flickrObject).to.have.own.property('apiKey').to.be.a('string');
+    expect(flickrObject).to.have.own.property('apiSecret').to.be.a('string');
+    expect(flickrObject).to.have.own.property('accessToken').to.be.a('string');
+    expect(flickrObject).to.have.own.property('accessTokenSecret').to.be.a('string');
+  });
 
-//   it('should have a Wallpaper array', () => {
-//     expect(configFile).to.have.own.property('wallpapers').to.be.a('array').to.not.be.empty;
-//   });
+  it('should have a Wallpaper array', () => {
+    expect(configFile).to.have.own.property('wallpapers').to.be.a('array');
+  });
 
-//   it('should have a Gallery array', () => {
-//     expect(configFile).to.have.own.property('galleries').to.be.a('array').to.not.be.empty;
-//   });
-// });
+  it('should have a Gallery array', () => {
+    expect(configFile).to.have.own.property('galleries').to.be.a('array');
+  });
+});
 
 
 describe('#flickrInterface', () => {

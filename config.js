69062654ccd362c208e01d45b4b073e83d720c8b
David Lampon <david.lampon@netcentric.biz>
2017-06-29 11:32:42 +0200

    Added generic config.js to pass the Travis CI

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  flickr: {
+    apiKey: '',
+    apiSecret: '',
+    accessToken: '',
+    accessTokenSecret: '',
+  },
+  personalId: '110364522@N02',
+  wallpapers: [
+    '72157649120706314', // New Zealand
+    '72157651043152220', // Copenhaguen
+  ],
+  galleries: [
+    '72157667307121933', // Family
+  ],
+  tags: [
+    'eric',
+    'sandra',
+    'family',
+  ],
+};
